1b586f4c36d3325aaa2760c7d568d4495c65e445
Bukola Adeniyan <87163943+Queensdelight@users.noreply.github.com>
2024-10-31 12:52:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ with ui.sidebar(open="open"):
 
     ui.a(
         "GitHub",
-        href= "https://github.com/SchroderJ-pixel/cintel-02-data-SchroderJ-pixel",
+        href= "https://github.com/Queensdelight/cintel-02-data/tree/main",
         target="_blank"
     )
 
